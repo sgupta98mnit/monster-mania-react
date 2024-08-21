28814fe73d13fd7a9cb27674a7f3cf826a954882
LAPTOP-NB3KLFFF\sgupt <sgupta98mnit@gmail.com>
2024-08-20 22:39:21 -0400

    changed function component to class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: "Hello Sumit"
+    }
+  }
+  render() {
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.string}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by Sumit
-        </a>
+        <button onClick={() => this.setState({string: "Hello Sumit 1"})}></button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
