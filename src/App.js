8fd7488409bb59870723118f54b5b3e9834363b5
Sumit Gupta <sgupta98mnit@gmail.com>
2025-07-13 17:28:58 -0400

    Format code

--- src/App.js
+++ src/App.js
@@ -14,16 +14,17 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({monsters: users}))
+      .then(users => this.setState({ monsters: users }))
   }
+
   render() {
-    return(
+    return (
       <div className="App">
         <CardList>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))
-        }
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+          }
         </CardList>
       </div>
     );
