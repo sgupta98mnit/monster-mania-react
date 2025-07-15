8668dd68b264937e433ab686cb078f40e22e72e7
Sumit Gupta <sgupta98mnit@gmail.com>
2025-07-14 20:31:58 -0400

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -17,11 +18,24 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState({ searchField });
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
+        <input type="search" placeholder="search monsters" className='search-box' onChange={onSearchChange}/>
         <CardList>
-          {this.state.monsters.map(monster => (
+          {filteredMonsters.map(monster => (
             <h1 key={monster.id}>{monster.name}</h1>
           ))
           }
