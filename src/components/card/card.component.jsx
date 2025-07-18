bd02aa3e3e1ad7becca0cb2bee39c64608f968c3
Sumit Gupta <sgupta98mnit@gmail.com>
2025-07-17 20:29:37 -0400

    Convert seacrhbox into functional component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,8 @@ import { Component } from "react";
 
 import './card.styles.css';
 
-const Card = ({ id, name, email }) => {
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
   return (
     <div className='card-container' key={id}>
       <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,15 @@
 import { Component } from 'react';
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-  render() {
-    const { placeholder, className, onChangeHandler } = this.props;
-    return (
-      <input
-        type="search"
-        placeholder={placeholder}
-        className={`search-box ${className}`}
-        onChange={onChangeHandler}
-      />
-    );
-  }
+const SearchBox = ({ placeholder, className, onChangeHandler }) => {
+  return (
+    <input
+      type="search"
+      placeholder={placeholder}
+      className={`search-box ${className}`}
+      onChange={onChangeHandler}
+    />
+  );
 }
 
 export default SearchBox;
