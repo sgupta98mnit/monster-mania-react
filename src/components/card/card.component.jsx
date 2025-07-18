6fa2535071bd13efb031a41f96829799d2e87351
Sumit Gupta <sgupta98mnit@gmail.com>
2025-07-17 20:26:19 -0400

    Convert card into functional component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,16 +2,13 @@ import { Component } from "react";
 
 import './card.styles.css';
 
-class Card extends Component {
-    render() {
-        const { id, name, email } = this.props.monster;
-        return (
-            <div className='card-container' key={id}>
-              <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
-    }
+const Card = ({ id, name, email }) => {
+  return (
+    <div className='card-container' key={id}>
+      <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
 }
 export default Card; 
